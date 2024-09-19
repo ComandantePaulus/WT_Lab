48deb759a775fb54a9d23aa9f6c8e11bbbac1368
ComandantePaulus <46773362+ComandantePaulus@users.noreply.github.com>
2024-09-19 13:00:52 +0300

    Initial Upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WT_Lab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
